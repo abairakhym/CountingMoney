b59d9d2268d9dbd95135e4a508c4a64a8fc1e88b
Abai <rakhym.abai@mail.ru>
2022-08-26 16:20:12 +0600

    [feature] update transaction

---
+++ app/src/main/java/com/example/countingmoney/view/EditTransactionFragment.kt
@@ -0,0 +1,78 @@
+package com.example.countingmoney.view
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.countingmoney.MainActivity
+import com.example.countingmoney.R
+import com.example.countingmoney.databinding.FragmentEditTransactionBinding
+import com.example.countingmoney.model.Transaction
+import com.example.countingmoney.utils.Constants
+import com.example.countingmoney.utils.binding.BindingFragment
+
+class EditTransactionFragment : BindingFragment<FragmentEditTransactionBinding>(
+    FragmentEditTransactionBinding::inflate) {
+
+    lateinit var viewModel: TransactionViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        val getTransaction: Transaction = arguments?.get(Constants.KEY_WORD) as Transaction
+
+        binding.apply {
+            etTitle.setText(getTransaction.title)
+            etAmount.setText(getTransaction.amount.toString())
+            val transactionType = when(getTransaction.transaction_type) {
+                "Expenses" -> 0
+                "Income" -> 1
+                else -> {
+                    Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+            spnTransactionType.setSelection(transactionType as Int)
+            val tag = when(getTransaction.category) {
+                "Entertaiment" -> 0
+                "Transportation" -> 1
+                "Cafe" -> 2
+                "Groceries" -> 3
+                "Workout" -> 4
+                "Investments" -> 5
+                "Other" -> 6
+                else -> {
+                    Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()}
+            }
+            spnTag.setSelection(tag as Int)
+            btnEditDate.setText(getTransaction.time)
+            etDescription.setText(getTransaction.description)
+
+
+
+            btnEditTransaction.setOnClickListener {
+                val transaction = Transaction(id = getTransaction.id,
+                    amount = etAmount.text.toString().toDouble(),
+                    title = etTitle.text.toString(),
+                    description = etDescription.text.toString(),
+                    transaction_type = spnTransactionType.selectedItem.toString(),
+                    category = spnTag.selectedItem.toString(),
+                    time = "husdak")
+
+                when (transaction.category) {
+                    "Entertaiment" -> {transaction.image = R.drawable.entertaiment}
+                    "Transportation" -> {transaction.image = R.drawable.transportation}
+                    "Cafe" -> {transaction.image = R.drawable.cafe}
+                    "Groceries" -> {transaction.image = R.drawable.groceries}
+                    "Workout" -> {transaction.image = R.drawable.workout}
+                    "Investments" -> {transaction.image = R.drawable.investing}
+                    "Other" -> {transaction.image = R.drawable.other}
+                }
+                Log.d("TAG", "onViewCreated: ")
+                viewModel.updateTransaction(transaction)
+                findNavController().navigate(R.id.action_editTransactionFragment_to_mainFragment)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/countingmoney/view/TransactionDetailFragment.kt
+++ app/src/main/java/com/example/countingmoney/view/TransactionDetailFragment.kt
@@ -2,6 +2,8 @@ package com.example.countingmoney.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.example.countingmoney.R
 import com.example.countingmoney.databinding.FragmentTransactionDetailBinding
 import com.example.countingmoney.model.Transaction
 import com.example.countingmoney.utils.Constants
@@ -20,6 +22,12 @@ class TransactionDetailFragment : BindingFragment<FragmentTransactionDetailBindi
             tvTagDetail.text = transaction.category
             tvTimeDetail.text = transaction.time
             tvNoteDetail.text = transaction.description
+            val bundle = Bundle().apply {
+                putSerializable(Constants.KEY_WORD, transaction)
+            }
+            btnEditDetail.setOnClickListener {
+                findNavController().navigate(R.id.action_transactionDetailFragment_to_editTransactionFragment, bundle)
+            }
         }
     }
 }
